be7b2c539c6448e861b056d67d619003b5e57f67
Ayush <ayushthakur3001@gmail.com>
2021-06-19 09:39:55 +0530

    updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,21 +1,28 @@
 import { useState } from 'react'
 import './contact.scss'
 const Contact = () => {
+
+    const [msg , setMsg]  = useState(false);
     const [name, setName] = useState("");
     const [mail, setMail] = useState("");
+    const [comment, setComment] = useState("");
+
+    const handleSubmit = ()=>{
+        setMsg(true);
+    }
 
     return (
 
 
-
+        <>
         <div className="contact" id="contact">
             <div className="left">
                 <img src="./assets/contact-me.png" alt="img.." />
             </div>
             <div className="right">
                 <h1>Let's Talk</h1>
-               
-                <form>
+                
+                <form methond="POST" onSubmit={handleSubmit}>
                     
                         
                         <input type="text" placeholder="Full Name" value={name} onChange={(e) => {
@@ -28,17 +35,25 @@ const Contact = () => {
                         }} />
 
                        
-                        <textarea cols="30" rows="10" placeholder="Comments" ></textarea>
+                        <textarea cols="30" rows="10" placeholder="Comments"
+                        value={comment} onChange={(e)=>{
+                            setComment(e.target.value)
+                        }} ></textarea>
                         
 
                     
                     
                 </form>
+                <button className="btn" onClick={handleSubmit}   >Send</button>
+                {msg && <span>Thanks, I'll reply ASAP </span>}
                 
 
             </div>
 
         </div>
+
+        <script src="https://smtpjs.com/v3/smtp.js" ></script>
+        </>
     )
 }
 
